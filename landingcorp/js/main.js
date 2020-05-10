877ff4defee4675d5615dca5bc8ce327a3af50f4
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-05-10 14:02:25 +0600

    Add product scroll functionality

---
+++ landingcorp/js/main.js
@@ -0,0 +1,38 @@
+let productMenuItem = document.querySelectorAll(
+  ".product_menu_list .product_menu_item a"
+);
+let productDetails = document.querySelectorAll(
+  ".product_section .product_details"
+);
+let productMenuList = document.querySelector(".product_menu_list");
+let lastId;
+let cur = [];
+
+productMenuItem.forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+    let target = document.querySelector(event.target.hash);
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+});
+
+window.addEventListener("scroll", (event) => {
+  let fromTop = window.scrollY;
+  productMenuList.classList.add("hello");
+
+  productMenuItem.forEach((link) => {
+    let section = document.querySelector(link.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      link.classList.add("current");
+    } else {
+      link.classList.remove("current");
+    }
+  });
+});
