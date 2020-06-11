31c87d647861fdf28b280bd71159b30537fae96a
Mushfiqur Niloy <mushfiqur06@gmail.com>
2020-06-11 19:19:53 +0600

    bug fixing

--- landingcorp/js/main.js
+++ landingcorp/js/main.js
@@ -1,10 +1,14 @@
 let productMenuItem = document.querySelectorAll(
-  ".product_menu_list .product_menu_item a"
+  ".product_list ul .product_menu_item a"
 );
+let productImageItem = document.querySelectorAll(
+  ".product_area .product_img_list ul li a"
+);
+
 let productDetails = document.querySelectorAll(
   ".product_section .product_details"
 );
-let productMenuList = document.querySelector(".product_menu_list");
+let productMenuList = document.querySelector(".product_list ul");
 let lastId;
 let cur = [];
 
@@ -21,11 +25,10 @@ productMenuItem.forEach((link) => {
 
 window.addEventListener("scroll", (event) => {
   let fromTop = window.scrollY;
-  productMenuList.classList.add("hello");
 
   productMenuItem.forEach((link) => {
     let section = document.querySelector(link.hash);
-
+    console.log(section.offsetTop);
     if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
@@ -35,6 +38,43 @@ window.addEventListener("scroll", (event) => {
       link.classList.remove("current");
     }
   });
+
+  productImageItem.forEach((links) => {
+    let section = document.querySelector(links.hash);
+
+    if (
+      section.offsetTop <= fromTop &&
+      section.offsetTop + section.offsetHeight > fromTop
+    ) {
+      links.classList.add("active");
+    } else {
+      links.classList.remove("active");
+    }
+  });
 });
 
-let navbarToggler = document.getElementById("navbar_toggler");
+// let navbarToggler = document.getElementById('navbar_toggler');
+
+// Navbar Link
+// let overviewLink = document.querySelector('.overviewLink');
+// let provisionLink = document.querySelector('.provisionLink');
+// let secureLink = document.querySelector('.secureLink');
+// let connectLink = document.querySelector('.connectLink');
+// let runLink = document.querySelector('.runLink');
+
+// // Section List
+// let overview = document.getElementById('overview');
+// let provision = document.getElementById('provision');
+// let secure = document.getElementById('secure');
+// let connect = document.getElementById('connect');
+// let run = document.getElementById('run');
+
+// let productSection = document.getElementsByClassName('product_section');
+
+// let overviewOffset = overview.offsetTop;
+
+// window.addEventListener('scroll', function () {
+// 	let isTop = window.scrollY < 100;
+
+// 	console.log(isTop);
+// });
